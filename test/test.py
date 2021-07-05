3e99fcce7c1fb7a0a43d1774add5454f7787f624
Palaash Atri <palaash.atri@gmail.com>
2021-07-05 14:12:15 +0530

    update test

--- test/test.py
+++ test/test.py
@@ -2,7 +2,13 @@ import pytest
 from module_package import convert_digital, convert_weight, convert_length, convert_area
 
 def test_convert_digital():
-    assert convert_digital.convert(1,"MB","bit") == 8000000
+    assert convert_digital.convert(1,"kB","byte") == 1000
 
 def test_convert_weight():
     assert convert_weight.convert(1,"kg","g") == 1000
+
+def test_convert_length():
+    assert convert_length.convert(1000,"cm","m") == 10
+
+def test_convert_area():
+    assert convert_area.convert(1,"sqm","sqcm") == 10000
